6462b5ba7db9ba1f64d9fbd6443268afbd2d2dd1
niko <qweqwe@gmail.com>
2024-03-14 23:20:37 +0400

    commit

---
+++ day 12/homework/day12.py
@@ -0,0 +1,35 @@
+point=int(input("enter your"))
+
+
+
+# if point > 90 and point < 100:
+#     print("დაფინანსდით")
+
+
+
+# elif point > 70 and point < 80:
+#     print("You have grant of 1500$")
+
+
+# elif point > 40 and point < 70:
+#     print("grant is 500")
+
+
+# else:
+#     print("no grant")
+
+
+if point == 10:
+    print("შენი შვილი კარგად სწავლობს")
+
+
+elif point == 9 or point ==8:
+    print("ცოტა მოუმატოს სწავლას")
+
+elif point == 5:
+    print("შენი შვილი არ სწავლობს და სწავლას მოუმატოს")
+
+
+else:
+    print("თქვენი შვილი გავარდა სკლიდან")
+
